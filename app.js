813517a6b081e474a5b7b083c6d7e35b613865ac
Tomislav Busic <tomislav.busic89@gmail.com>
2021-04-12 21:44:26 +0200

    intro text

--- app.js
+++ app.js
@@ -164,8 +164,15 @@ const game = () => {
 game();
 
 var typed = new Typed(".typing", {
-    strings: ["Rock paper and scissors", "Paper rock and scissors", "Scissors paper and rock"],
+    strings: ["Rock paper scissors",
+     "is a hand game usually played between two people", 
+     "in which each player simultaneously forms one of three shapes with an outstretched hand",
+     " These shapes are 'rock' (a closed fist),",
+     "'paper' (a flat hand),",
+     " and 'scissors' (a fist with the index finger and middle finger extended, forming a V).",
+     "'Scissors' is identical to the two-fingered V sign (also indicating 'victory' or 'peace')",
+     "except that it is pointed horizontally instead of being held upright in the air."],
     typeSpeed: 100,
-    backSpeed: 60,
+    backSpeed: 30,
     loop: true
 });
