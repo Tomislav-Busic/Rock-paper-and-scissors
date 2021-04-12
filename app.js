98e14b27f75131450f190dd4152363de52010e34
Tomislav Busic <tomislav.busic89@gmail.com>
2021-04-12 16:38:25 +0200

    promjena dizajna, animacije

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const tl = gsap.timeline({defaults: { ease: "power1.out" } }); //koliko dugo želimo da se svaka animacija dogodi
+
+tl.to(".text", { y: "0%", duration: 2, stagger: 0.35});
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5});
+tl.to(".intro1", { y: "-100%", duration: 1}, "-=1");
+
 const game = () => {
     let pScore = 0;
     let cScore = 0;
@@ -18,6 +24,13 @@ const game = () => {
         const options = document.querySelectorAll(".options button");
         const playerHand = document.querySelector(".player-hand");
         const computerHand = document.querySelector(".computer-hand");
+        const hands = document.querySelectorAll('.hands img');
+
+        hands.forEach(hand => {
+            hand.addEventListener('animationend', function(){
+                this.style.animation = '';
+            })
+        })
         //computer options
         const computerOptions = ["rock", "paper", "scissors"];
 
@@ -26,11 +39,17 @@ const game = () => {
                 //computer choice
                 const computerNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions[computerNumber];
-                //Here is where we call compare hands
-                compareHands(this.textContent, computerChoice);
-                //Update Images
-                playerHand.src = `./assets/${this.textContent}.png`;
-                computerHand.src = `./assets/${computerChoice}.png`;
+                
+                setTimeout(() => {
+                    //Here is where we call compare hands
+                    compareHands(this.textContent, computerChoice);
+                    //Update Images
+                    playerHand.src = `./assets/${this.textContent}.png`;
+                    computerHand.src = `./assets/${computerChoice}.png`;
+                }, 2000);
+
+                playerHand.style.animation = "shakePlayer 2s ease";
+                computerHand.style.animation = "shakeComputer 2s ease";
             });
         });
     };
@@ -50,6 +69,7 @@ const game = () => {
         //Checking for a tie
         if(playerChoice === computerChoice){
             winner.textContent = "It is a tie";
+            winner.style.color = "yellow";
             return;
         }
 
@@ -57,11 +77,13 @@ const game = () => {
         if(playerChoice === "rock"){
             if(computerChoice === "scissors"){
                 winner.textContent = "Player Wins";
+                winner.style.color = "green";
                 pScore++;
                 updateScore();
                 return;
             }else{
                 winner.textContent = "Computer Wins";
+                winner.style.color = "crimson";
                 cScore++;
                 updateScore();
                 return;
@@ -72,11 +94,13 @@ const game = () => {
         if(playerChoice === "paper"){
             if(computerChoice === "rock"){
                 winner.textContent = "Player Wins";
+                winner.style.color = "green";
                 pScore++;
                 updateScore();
                 return;
             }else{
                 winner.textContent = "Computer Wins";
+                winner.style.color = "crimson";
                 cScore++;
                 updateScore();
                 return;
@@ -87,11 +111,13 @@ const game = () => {
         if(playerChoice === "scissors"){
             if(computerChoice === "paper"){
                 winner.textContent = "Player Wins";
+                winner.style.color = "green";
                 pScore++;
                 updateScore();
                 return;
             }else{
                 winner.textContent = "Computer Wins";
+                winner.style.color = "crimson";
                 cScore++;
                 updateScore();
                 return;
